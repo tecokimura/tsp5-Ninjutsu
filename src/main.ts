f85a62ccc8017d172d87fc73eeb6a2cf9682bdaf
Kimura Kaz <kimura@tecotecnoMacBook-Pro.local>
2021-12-08 22:58:12 +0900

    シーン切り替え管理クラス追加

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,63 @@
 import p5 from "p5";
-import　{Def} from "./def";
-import {SceneManager} from "./SceneManager";
+import {Def} from "./def";
+import {Scene} from "./scene";
+
+let scene = null;
+let isDraw = false;
 
 const sketch = (p: p5) => {
-  p.setup = () => {
-    p.createCanvas(480, 480);
-    
-    // 定期的に実行する
-    repeatProc();
-  };
+    p.setup = () => {
+        p.createCanvas(480, 480);
 
-  p.preload = () => {
-      
-  };
+        scene = new Scene();
 
-  p.draw = () => {
-    p.background(220);
-    p.ellipse(50, 50, 80, 80);
-  };
+        proc();
+    };
 
-  function repeatProc(time:number=10000) {
-    setTimeout(() => { proc(); }, time);
-  };
+    p.preload = () => {
+        // 画像読み込み予定
+    };
 
-  function proc() {
-      console.log("in proc");
-      repeatProc();
+    p.draw = () => {
+        if( isDraw ) {
+            if( scene.is(Scene.INIT)) {
+                p.background(220);
+                p.ellipse(50, 50, 280, 280);
+            }
+            else
+            if( scene.is(Scene.LOADING)) {
+                p.background(20);
+                p.ellipse(150, 150, 80, 80);
+            }
+
+            console.log("in draw");
+        }
+    };
+
+    function repeatProc(time:number=100) {
+        setTimeout(() => { proc(); }, time);
+    };
+
+    function proc() {
+        isDraw = false;
+        console.log("in proc");
+
+        if( scene.is(Scene.INIT) ) {
+            console.log("Scene.INIT");
+            if(scene.frame > 10) {
+                scene.set(Scene.LOADING);
+            }
+        }
+        else
+        if( scene.is(Scene.LOADING) ) {
+            console.log("Scene.LOADING");
+
+        }
+
+        scene.count();
+
+        isDraw = true;
+        repeatProc();
     };
 };
 
