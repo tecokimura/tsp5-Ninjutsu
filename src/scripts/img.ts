b92e11eba3d77f54d451cd237986b6491783097c
Kimura Kaz <kimura@tecotecnoMacBook-Pro.local>
2021-12-20 09:33:40 +0900

    complete close #1

--- src/scripts/img.ts
+++ src/scripts/img.ts
@@ -12,29 +12,28 @@ export class Img {
     }
 
     preload() {
-        // ここの結果から読み込み方を調べる
         console.log(IMG_PNGS);
 
-        // this.images = this.p.loadImage(IMG_PNGS[Img.NINJA_STAND]);
+        this.images = this.p.loadImage(IMG_PNGS[Img.NINJA_STAND]);
     }
 
     drawImage(index:number, x:number, y:number) {
-        // this.p.image(this.images,x,y);
+        this.p.image(this.images,x,y);
     }
 
 
-
-    static NINJA_CRASH:string       = 'cd1-ninja-crash.png'
-    static NINJA_DOWN_1:string      = 'nd1-ninja-down1.png'
-    static NINJA_DOWN_2:string      = 'nd2-ninja-down2.png'
-    static NINJA_FLY_L:string       = 'nf1-ninja-fly1.png'
-    static NINJA_FLY_C:string       = 'nf2-ninja-fly2.png'
-    static NINJA_FLY_R:string       = 'nf3-ninja-fly3.png'
-    static NINJA_JUTSU_BASE:string  = 'nj1-ninja-jutsu-base.png'
-    static NINJA_JUTSU_GU:string    = 'nj2-ninja-jutsu-gu.png'
-    static NINJA_JUTSU_CHOKI:string = 'nj3-ninja-jtsutu-choki.png'
-    static NINJA_JUTSU_PA:string    = 'nj4-ninja-jutsu-pa.png'
-    static NINJA_STAND:string       = 'ns1-ninja-stand.png'
+    // 読み込み用配列にしてインデックスを別途定義する
+    static NINJA_CRASH:string       = 'cd1-ninja-crash'
+    static NINJA_DOWN_1:string      = 'nd1-ninja-down1'
+    static NINJA_DOWN_2:string      = 'nd2-ninja-down2'
+    static NINJA_FLY_L:string       = 'nf1-ninja-fly1'
+    static NINJA_FLY_C:string       = 'nf2-ninja-fly2'
+    static NINJA_FLY_R:string       = 'nf3-ninja-fly3'
+    static NINJA_JUTSU_BASE:string  = 'nj1-ninja-jutsu-base'
+    static NINJA_JUTSU_GU:string    = 'nj2-ninja-jutsu-gu'
+    static NINJA_JUTSU_CHOKI:string = 'nj3-ninja-jtsutu-choki'
+    static NINJA_JUTSU_PA:string    = 'nj4-ninja-jutsu-pa'
+    static NINJA_STAND:string       = 'ns1-ninja-stand'
 
 
 }
