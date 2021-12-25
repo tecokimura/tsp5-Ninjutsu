1acbfe6b1e5e7bbff9d1df5ed5b470db9a3aa073
Wacco <tecokimura@gmail.com>
2021-12-25 17:19:22 +0900

    #3 load all images (#4)
    * add setup,run
    
    * SUCCESS all PNG Images
    
    * add enemy images
    
    * Fix close #3
    
    Co-authored-by: 木村 和幸 <kimura.kazuyuki@tecotec.co.jp>
    Co-authored-by: Kimura Kaz <kimura@tecotecnoMacBook-Pro.local>

--- src/scripts/img.ts
+++ src/scripts/img.ts
@@ -1,39 +1,81 @@
 import p5 from "p5";
 
 //@ts-ignore
-import IMG_PNGS from "../images/*.png";
+import DIST_IMG_PNGS from "../images/*.png";
 
 export class Img {
     p:p5 = null;
-    images = null;
+    images:p5.Image[] = new Array(Img.IMAGE_MAX);
 
     constructor(p5: p5) {
         this.p = p5;
     }
 
     preload() {
-        console.log(IMG_PNGS);
+        console.log(DIST_IMG_PNGS);
 
-        this.images = this.p.loadImage(IMG_PNGS[Img.NINJA_STAND]);
+        // 全ての画像を読み込む
+        for(let i=0;i<Img.PNG_FNAMES.length; i++) {
+            this.images[i] = (this.p.loadImage(DIST_IMG_PNGS[Img.PNG_FNAMES[i]]));
+        }
     }
 
     drawImage(index:number, x:number, y:number) {
-        this.p.image(this.images,x,y);
+        this.p.image(this.images[index],x,y);
     }
 
 
-    // 読み込み用配列にしてインデックスを別途定義する
-    static NINJA_CRASH:string       = 'cd1-ninja-crash'
-    static NINJA_DOWN_1:string      = 'nd1-ninja-down1'
-    static NINJA_DOWN_2:string      = 'nd2-ninja-down2'
-    static NINJA_FLY_L:string       = 'nf1-ninja-fly1'
-    static NINJA_FLY_C:string       = 'nf2-ninja-fly2'
-    static NINJA_FLY_R:string       = 'nf3-ninja-fly3'
-    static NINJA_JUTSU_BASE:string  = 'nj1-ninja-jutsu-base'
-    static NINJA_JUTSU_GU:string    = 'nj2-ninja-jutsu-gu'
-    static NINJA_JUTSU_CHOKI:string = 'nj3-ninja-jtsutu-choki'
-    static NINJA_JUTSU_PA:string    = 'nj4-ninja-jutsu-pa'
-    static NINJA_STAND:string       = 'ns1-ninja-stand'
+    //===========================
+    // File読み込み用
+    // ls -1 src/images | sed -e 's/.png$//'
+    // を貼れるように並べる
+    static PNG_FNAMES:string[] = [
+        'e0b1-bird1',
+        'e0b2-bird2',
+        'e1k1-kunai1',
+        'e1k2-kunai2',
+        'e3s1-shuri1',
+        'e3s2-shuri2',
+        'e4k2-shinobi2',
+        'e4s1-shinobi1',
+        'e3s1-shuri1',
+        'e3s2-shuri2',
+        'n00s1-ninja-stand',
+        'n10j1-ninja-jutsu-base',
+        'n10j2-ninja-jutsu-gu',
+        'n10j3-ninja-jtsutu-choki',
+        'n10j4-ninja-jutsu-pa',
+        'n20f1-ninja-fly1',
+        'n20f2-ninja-fly2',
+        'n20f3-ninja-fly3',
+        'n30c1-ninja-crash',
+        'n40d1-ninja-down1',
+        'n40d2-ninja-down2'
+    ]
 
+    // 読み込み用配列にしてインデックスを別途定義する
+    static ENEMY_BIRD:number        =0; 
+    static ENEMY_BIRD1:number       =1;  
+    static ENEMY_KUNAI:number       =2;  
+    static ENEMY_KUNAI1:number      =3;  
+    static ENEMY_SHURIKEN:number    =4;  
+    static ENEMY_SHURIKEN1:number   =5;  
+    static ENEMY_SHINOBI:number     =6;  
+    static ENEMY_SHINOBI1:number    =7;  
+    static ENEMY_UFO:number         =8;  
+    static ENEMY_UFO1:number        =9;  
+    static NINJA_STAND:number       =10; 
+    static NINJA_JUTSU_BASE:number  =11; 
+    static NINJA_JUTSU_GU:number    =12; 
+    static NINJA_JUTSU_CHOKI:number =13; 
+    static NINJA_JUTSU_PA:number    =14; 
+    static NINJA_FLY_L:number       =15; 
+    static NINJA_FLY_C:number       =16; 
+    static NINJA_FLY_R:number       =17; 
+    static NINJA_CRASH:number       =18; 
+    static NINJA_DOWN:number        =19; 
+    static NINJA_DOWN2:number       =20; 
+    static IMAGE_MAX:number         =21;
+    //===========================
 
 }
