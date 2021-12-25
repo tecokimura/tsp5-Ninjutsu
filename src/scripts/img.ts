99b5268731ea0570f3eebd1134eb620ea36bee8f
Kimura Kaz <kimura@tecotecnoMacBook-Pro.local>
2021-12-25 16:53:31 +0900

    SUCCESS all PNG Images

--- src/scripts/img.ts
+++ src/scripts/img.ts
@@ -1,39 +1,59 @@
 import p5 from "p5";
 
 //@ts-ignore
-import IMG_PNGS from "../images/*.png";
+import DIST_IMG_PNGS from "../images/*.png";
 
 export class Img {
     p:p5 = null;
-    images = null;
+    images:p5.Image[] = new Array(Img.IMAGE_MAX);
 
     constructor(p5: p5) {
         this.p = p5;
     }
 
     preload() {
-        console.log(IMG_PNGS);
+        console.log(DIST_IMG_PNGS);
 
-        this.images = this.p.loadImage(IMG_PNGS[Img.NINJA_STAND]);
+        // 全ての画像を読み込む
+        for(let i=0;i<Img.PNG_FNAMES.length; i++) {
+            this.images[i] = (this.p.loadImage(DIST_IMG_PNGS[Img.PNG_FNAMES[i]]));
+        }
     }
 
     drawImage(index:number, x:number, y:number) {
-        this.p.image(this.images,x,y);
+        this.p.image(this.images[0],x,y);
     }
 
 
+    // ls -1 を貼れるように並べる
+    static PNG_FNAMES:string[] = [
+        'n00s1-ninja-stand',
+        'n10j1-ninja-jutsu-base',
+        'n10j2-ninja-jutsu-gu',
+        'n10j3-ninja-jtsutu-choki',
+        'n10j4-ninja-jutsu-pa',
+        'n20f1-ninja-fly1',
+        'n20f2-ninja-fly2',
+        'n20f3-ninja-fly3',
+        'n30c1-ninja-crash',
+        'n40d1-ninja-down1',
+        'n40d2-ninja-down2'
+    ]
+
     // 読み込み用配列にしてインデックスを別途定義する
-    static NINJA_CRASH:string       = 'cd1-ninja-crash'
-    static NINJA_DOWN_1:string      = 'nd1-ninja-down1'
-    static NINJA_DOWN_2:string      = 'nd2-ninja-down2'
-    static NINJA_FLY_L:string       = 'nf1-ninja-fly1'
-    static NINJA_FLY_C:string       = 'nf2-ninja-fly2'
-    static NINJA_FLY_R:string       = 'nf3-ninja-fly3'
-    static NINJA_JUTSU_BASE:string  = 'nj1-ninja-jutsu-base'
-    static NINJA_JUTSU_GU:string    = 'nj2-ninja-jutsu-gu'
-    static NINJA_JUTSU_CHOKI:string = 'nj3-ninja-jtsutu-choki'
-    static NINJA_JUTSU_PA:string    = 'nj4-ninja-jutsu-pa'
-    static NINJA_STAND:string       = 'ns1-ninja-stand'
+    // ls -1の結果に合わせてインデックスを定義
+    static NINJA_STAND:number       = 0;
+    static NINJA_JUTSU_BASE:number  = 1;
+    static NINJA_JUTSU_GU:number    = 2;
+    static NINJA_JUTSU_CHOKI:number = 3;
+    static NINJA_JUTSU_PA:number    = 4;
+    static NINJA_FLY_L:number       = 5;
+    static NINJA_FLY_C:number       = 6;
+    static NINJA_FLY_R:number       = 7;
+    static NINJA_CRASH:number       = 8;
+    static NINJA_DOWN_1:number      = 9;
+    static NINJA_DOWN_2:number      = 10;
+    static IMAGE_MAX:number         = 11;
 
 
 }
