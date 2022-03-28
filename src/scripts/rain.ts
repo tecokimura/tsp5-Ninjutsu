265ad351dc062b11694ccd8dc249dddab9a68384
Wacco <tecokimura@gmail.com>
2022-03-28 20:55:33 +0900

    add rains & snow

--- src/scripts/rain.ts
+++ src/scripts/rain.ts
@@ -4,7 +4,7 @@ import {BgObj} from "./bgobj";
 import {Def} from "./def";
 import {Util} from "./util";
 
-export class Snow extends BgObj{
+export class Rain extends BgObj{
 
     constructor() {
         super();
@@ -27,43 +27,42 @@ export class Snow extends BgObj{
     add(type:number=Def.TYPE_ALL) {
         let isAdd = false;
         if(this.isEnable() == false) {
-            this.init();
-            this.posY = -40-(Util.getRandInt()%120);
-            this.posX = Util.getRandInt()%Def.DISP_W;
-            this.spX = 0;
-            this.spY = Util.getRandInt()%5+2;
 
             if(type == Def.TYPE_ALL) {
                 type = Util.getRandInt()%Def.TYPE_ALL;
             }
 
+            this.init();
+            this.posY = -40-(Util.getRandInt()%120);
+            this.posX = Util.getRandInt()%Def.DISP_W;
+
             // ランダムでサイズを決める
-            this.width = Util.getRandInt(10)+2;
-            this.height= this.width;
-            this.alpha = 100;
+            this.width = 2;
+            this.height= Util.getRandInt(80)+10;
 
             this.status = Def.ST_PLAY;
 
+            this.alpha = Util.getRandInt(100)+30;
+            this.spY = Util.getRandInt()%20+10;
+
             isAdd = this.isEnable();
+
         }
 
         return isAdd;
     }
 
     drawBack(p5:p5) {
-        // drawCircle
         if( this.isEnable() && this.type != Def.TYPE_NEAR) {
-            p5.angleMode(p5.DEGREES);
-            p5.fill(200,200,200, this.alpha);
-            p5.arc(this.posX+this.width/2, this.posY+this.height/2, this.width, this.height, 0, 360);
+            p5.fill(0,98,160, this.alpha);
+            p5.rect(this.posX+this.width/2, this.posY+this.height/2, this.width, this.height );
         }
     }
 
     drawFront(p5:p5) {
         if( this.isEnable() && this.type == Def.TYPE_NEAR) {
-            p5.angleMode(p5.DEGREES);
-            p5.fill(255,255,255, this.alpha*2);
-            p5.arc(this.posX+this.width/2, this.posY+this.height/2, this.width, this.height, 0, 360);
+            p5.fill(51,153,204, this.alpha);
+            p5.rect(this.posX+this.width/2, this.posY+this.height/2, this.width, this.height );
         }
     }
 
