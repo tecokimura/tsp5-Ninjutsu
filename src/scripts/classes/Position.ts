77e67d9c2d949d32941ffca7f6e6a521646884c1
Kimura Kaz <tecokimura@gmail.com>
2022-10-16 10:31:35 +0900

    Begin refactoring with Position.ts

---
+++ src/scripts/classes/Position.ts
@@ -0,0 +1,27 @@
+// Player.ts, Enemy.ts, BgObj.ts をみて必要そうなものを実装する
+export class Position {
+    private x: number = 0;
+    private y: number = 0;
+
+    constructor(x, y) {
+        this.init(x, y);
+    }
+
+    init(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // 意味がないから本を確認する
+    setX(x: number) {
+        this.x = x;
+    }
+
+    setY(y: number) {
+        this.y = y;
+    }
+
+    move(x: number, y: number): Position {
+        return new Position(x, y);
+    }
+}
---
+++ src/scripts/classes/Spped.ts
@@ -0,0 +1,18 @@
+// 仮実装 先にPositionをやる
+export class Speed {
+    x: number = 0;
+    y: number = 0;
+
+    constructor(x, y) {
+        this.init(x, y);
+    }
+
+    init(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    move(x: number, y: number): Speed {
+        return new Speed(x, y);
+    }
+}
