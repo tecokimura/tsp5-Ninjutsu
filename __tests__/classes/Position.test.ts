6924ec6f1838bc7d1b2989d6d073d8b5792eb8b1
Wacco <tecokimura@gmail.com>
2022-10-28 13:59:53 +0900

    test Position.move(Speed)

--- __tests__/classes/Position.test.ts
+++ __tests__/classes/Position.test.ts
@@ -1,4 +1,5 @@
 import { Position } from "../../src/scripts/classes/Position";
+import { Speed } from "../../src/scripts/classes/Speed";
 
 test("constructor", () => {
     const pos = new Position(10 ,20);
@@ -8,6 +9,20 @@ test("constructor", () => {
 
 test("move", () => {
     const pos = new Position(10 ,20);
+    const sp = new Speed(1 ,2);
+
+    pos.move(sp);
+
+    expect(pos.getX()).toBe(11);
+    expect(pos.getY()).toBe(22);
+
+    //
+    sp.resetX(-1);
+    sp.resetY(-2);
+    
+    pos.move(sp);
+
     expect(pos.getX()).toBe(10);
     expect(pos.getY()).toBe(20);
+
 });
--- src/scripts/classes/Position.ts
+++ src/scripts/classes/Position.ts
@@ -1,5 +1,6 @@
-// TODO: テストを作る
-// Player.ts, Enemy.ts, BgObj.ts をみて必要そうなものを実装する
+import { Speed } from "./Speed";
+
+
 export class Position {
     private x: number = 0;
     private y: number = 0;
@@ -24,7 +25,8 @@ export class Position {
         this.y = y;
     }
 
-    move(x: number, y: number): Position {
-        return new Position(x, y);
+    move(sp:Speed) {
+        this.x += sp.getX();
+        this.y += sp.getY();
     }
 }
