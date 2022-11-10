932651b0b4935a234476c3d320c645705e11d4cc
Wacco <tecokimura@gmail.com>
2022-11-10 19:53:40 +0900

    prepare Graphics and Display class

---
+++ src/scripts/classes/Display.ts
@@ -0,0 +1,6 @@
+import { Size } from "./Basis/Size"
+
+// 画面サイズとかを管理
+export class Display extends Size{
+
+}
---
+++ src/scripts/classes/Graphics.ts
@@ -0,0 +1,17 @@
+import p5 from "p5"
+// 画像関連のクラス
+// p5js を受け取って色々やる
+export class Graphics {
+
+    p5js:p5
+
+    constructor(p:p5) {
+        this.p5js = p;
+    }
+
+    // drawXxx
+    // drawXxx
+    // drawXxx
+    // drawXxx
+    // drawXxx
+}
