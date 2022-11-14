d174aa96bc1ec070fa112ce78384e043da767411
Kimura Kaz <tecokimura@gmail.com>
2022-11-14 20:03:51 +0900

    update Display and Grapchis for game

--- src/scripts/classes/Display.ts
+++ src/scripts/classes/Display.ts
@@ -1,6 +1,29 @@
 import { Size } from "./Basis/Size"
+import { Position } from "./Basis/Position"
 
 // 画面サイズとかを管理
-export class Display extends Size{
+export class Display {
+    private offset: Position
+    private size: Size
 
+    constructor(w: number, h: number) {
+        this.offset = new Position()
+        this.size = new Size()
+    }
+
+    getX(): number {
+        return this.offset.getX()
+    }
+
+    getY(): number {
+        return this.offset.getY()
+    }
+
+    getWidth(): number {
+        return this.offset.getWidth()
+    }
+
+    getHeight(): number {
+        return this.size.getHeight()
+    }
 }
